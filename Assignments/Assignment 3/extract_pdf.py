2f783d9c71dc6f8afb689b451b007b960ce4fef1
boozie4 <90736536+boozie4@users.noreply.github.com>
2026-03-01 09:04:33 -0600

    Changes to be committed: 	deleted:    Assignments/Assignment 3/extract_pdf.py

--- Assignments/Assignment 3/extract_pdf.py
+++
@@ -1,8 +0,0 @@
-import PyPDF2
-path=r"c:\Users\ebooz\Downloads\MSSE642_Assignment3_v2.pdf"
-reader=PyPDF2.PdfReader(path)
-with open(r"c:\Users\ebooz\OneDrive\Desktop\MSSE642-2026Spring\MSSE642-2026Spring\Assignments\Assignment 3\Assignment3.txt","w",encoding="utf-8") as f:
-    for i,page in enumerate(reader.pages):
-        f.write("--- PAGE %d ---\n" % i)
-        f.write(page.extract_text() or "[no text]\n")
-print('done')
